c6917372a22edd26562d620fa6ca817271e24643
ChHrIzz <122598805+ChHrIzz@users.noreply.github.com>
2023-03-03 15:43:06 +0530

    Add files via upload
    fjfjf

---
+++ java/index.js
@@ -0,0 +1,34 @@
+//let fruits =["apple","grape","banana","orange"]
+//for(i=0;i<fruits.length;i++)
+//{
+//    console.log(fruits[i])
+//}
+//et fruits =["apple","grape","banana","orange"]
+//let i=0;
+//while(i,fruits.length){
+  //  console.log(fruits[i]);
+   // i++;
+//}
+//let p =("2" "4" "6" "7");
+//do{
+   // console.log(fruits[i])
+ //   i++;
+//}while(i<fru
+//if(p%2==0);
+//{
+//    console.log("even");
+//}
+//lse
+//{
+    let number=[2,3,4,7,8];
+    for(let i=0;i<number.length;i++){
+        if(number[i]%2==0){
+            console.log(`the given number ${number[i]} is even`);
+        }
+        else
+        {
+            console.log(`the given number ${number[i]} is odd`);
+        }
+    }
+
+//}
